126ee0b5898f095fca7c10c88c031f4c63f2eef6
brihamm22 <brihamm@gmail.com>
2025-11-19 16:15:20 +0000

    update 5

--- security-groups.tf
+++ security-groups.tf
@@ -20,7 +20,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
  
  # Allow 22,80 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.example.id
+  security_group_id = aws_security_group.brian-sec-grp.id
 
   cidr_ipv4   = "10.0.0.0/8"
   from_port   = 80
@@ -29,7 +29,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.example.id
+  security_group_id = aws_security_group.brian-sec-grp.id
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 22
