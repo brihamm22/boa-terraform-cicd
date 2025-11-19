ddb2ed5a30bf20118d0c4b5ae28edfe5029051ca
brihamm22 <brihamm@gmail.com>
2025-11-19 21:52:37 +0000

    finla

--- ec2.tf
+++ ec2.tf
@@ -13,3 +13,50 @@ resource "local_file" "ipadx" {
     content = aws_instance.example.public_ip
     filename = "${path.module}/myip.txt"
 }
+
+# Role
+resource "aws_iam_role" "brian_role" {
+  name = "brian_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2025-11-18"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+# Policy
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
